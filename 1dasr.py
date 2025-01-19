ccbe3c8243b0d3fa1a919b0a5bbdec55bd3c3cd5
Bobomurod2004 <bobomurodgoibnazarov@gmail.com>
2025-01-19 18:43:46 +0500

    xatolik

--- 1dasr.py
+++ 1dasr.py
@@ -19,20 +19,3 @@ class WebScrapingStudy:
         return response.json().get("data")
 
 
-
-    def run_scripit(self):
-        responses = self.get_json_data(self.url)
-        for response in responses:
-            name=response.get("name")
-            self.send_telegram_messenge(token=self.token, chat_id=self.chat_id,messenge=name)
-        return responses
-token ='7639005796:AAHU6moXHX_VtoR9AO-4WSAiqU12z0IDNiw'
-chat_id=5652442685
-
-url = "https://admin.soffstudy.uz/api/v1/gallery/?limit=9&offset=0"
-wss1 = WebScrapingStudy(url=url, token=token,chat_id=chat_id)
-
-
-x = wss1.run_scripit()
-
-print(x)
